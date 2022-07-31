1d827ebd7c623574cb3ddd507f02a9b607b59eae
Vlad Pomynchuk <pomynchuk123@gmail.com>
2022-08-01 02:12:39 +0300

    optimizes pictures

--- src/components/Hero/Hero.styled.jsx
+++ src/components/Hero/Hero.styled.jsx
@@ -1,5 +1,12 @@
 import styled from 'styled-components';
-import bgcImage from 'images/bgHero/pexels-alexandr-podvalny-1227513.jpeg';
+import mobile from 'images/bgHero/mobile.jpg';
+import mobile2x from 'images/bgHero/mobile@2x.jpg';
+import tablet from 'images/bgHero/tablet.jpg';
+import tablet2x from 'images/bgHero/tablet@2x.jpg';
+import desktop from 'images/bgHero/desktop.jpg';
+import desktop2x from 'images/bgHero/desktop@2x.jpg';
+import bigDesktop from 'images/bgHero/desktop+.jpg';
+import bigDesktop2x from 'images/bgHero/desktop+@2x.jpg';
 
 export const Section = styled.section`
   padding-top: 40px;
@@ -13,18 +20,85 @@ export const Section = styled.section`
       rgba(0, 0, 0, 0.5),
       rgba(0, 0, 0, 0.5)
     ),
-    url(${bgcImage});
+    url(${mobile});
   background-size: cover;
   background-position: center;
+
+  @media (min-device-pixel-ratio: 2),
+    (min-resolution: 192dpi),
+    (min-resolution: 2dppx) {
+    background-image: linear-gradient(
+        0deg,
+        rgba(0, 0, 0, 0.5),
+        rgba(0, 0, 0, 0.5)
+      ),
+      url(${mobile2x});
+  }
 
   @media screen and (min-width: 768px) {
     padding-top: 89px;
     padding-bottom: 88px;
+    background-image: linear-gradient(
+        0deg,
+        rgba(0, 0, 0, 0.5),
+        rgba(0, 0, 0, 0.5)
+      ),
+      url(${tablet});
+
+    @media (min-device-pixel-ratio: 2),
+      (min-resolution: 192dpi),
+      (min-resolution: 2dppx) {
+      background-image: linear-gradient(
+          0deg,
+          rgba(0, 0, 0, 0.5),
+          rgba(0, 0, 0, 0.5)
+        ),
+        url(${tablet2x});
+    }
   }
 
   @media screen and (min-width: 1024px) {
     padding-top: 164px;
     padding-bottom: 163px;
+    background-image: linear-gradient(
+        0deg,
+        rgba(0, 0, 0, 0.5),
+        rgba(0, 0, 0, 0.5)
+      ),
+      url(${desktop});
+
+    @media (min-device-pixel-ratio: 2),
+      (min-resolution: 192dpi),
+      (min-resolution: 2dppx) {
+      background-image: linear-gradient(
+          0deg,
+          rgba(0, 0, 0, 0.5),
+          rgba(0, 0, 0, 0.5)
+        ),
+        url(${desktop2x});
+    }
+  }
+
+  @media screen and (min-width: 1170px) {
+    padding-top: 164px;
+    padding-bottom: 163px;
+    background-image: linear-gradient(
+        0deg,
+        rgba(0, 0, 0, 0.5),
+        rgba(0, 0, 0, 0.5)
+      ),
+      url(${bigDesktop});
+
+    @media (min-device-pixel-ratio: 2),
+      (min-resolution: 192dpi),
+      (min-resolution: 2dppx) {
+      background-image: linear-gradient(
+          0deg,
+          rgba(0, 0, 0, 0.5),
+          rgba(0, 0, 0, 0.5)
+        ),
+        url(${bigDesktop2x});
+    }
   }
 `;
 
